9d3fff124341a8f2e39be90c7e2fa63c5d59ee9b
felipeomelo <felipe.o.melo@hotmail.com>
2024-10-25 12:28:04 -0300

    Implementação do listar autor

--- src/controladores/AutorControlador.ts
+++ src/controladores/AutorControlador.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express"
+import { autores } from "../bancoDeDados"
+
 
 export default class AutorControlador {
     listar(req: Request, res: Response){
-        return res.json()
+        return res.json(autores)
     }
 }
