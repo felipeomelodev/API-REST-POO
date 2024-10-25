cf78f113fdc7ab077444fd486b5b9b73c7da195b
felipeomelo <felipe.o.melo@hotmail.com>
2024-10-25 12:08:06 -0300

    Implementação do Banco de Dados em memória

---
+++ src/bancoDeDados.ts
@@ -0,0 +1,5 @@
+import Autor from "./modelos/Autor"
+import Post from "./modelos/Post"
+
+export const autores: Autor[] = []
+export const posts: Post[] = []
---
+++ src/controladores/AutorControlador.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express"
+
+export default class AutorControlador {
+    listar(req: Request, res: Response){
+        return res.json()
+    }
+}
