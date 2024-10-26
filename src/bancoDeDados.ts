ca518c388994fc6845c99911b291fb1059972c05
felipeomelo <felipe.o.melo@hotmail.com>
2024-10-26 11:17:44 -0300

    Implementação do listar postagem

--- src/bancoDeDados.ts
+++ src/bancoDeDados.ts
@@ -3,4 +3,13 @@ import Post from "./modelos/Post"
 
 export const autores: Autor[] = []
 
-export const posts: Post[] = []
+export const posts: Post[] = [
+    new Post({
+        titulo: 'Café é bom demais!',
+        descricao: 'Descrição do meu post',
+        autor: new Autor ({
+            nome: 'Martinha',
+            idade: 44 
+        })
+    })
+]
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express'
 import AutorControlador from './controladores/AutorControlador'
+import PostControlador from './controladores/PostControlador'
 
 const rotas = Router()
 
 const autorControlador = new AutorControlador()
+const postcontrolador = new PostControlador()
 
 rotas.get('/autores', autorControlador.listar)
 rotas.get('/autores/:id', autorControlador.detalhar)
 rotas.post('/autores', autorControlador.cadastrar)
 rotas.put('/autores/:id', autorControlador.editar)
 rotas.delete('/autores/:id', autorControlador.excluir)
+
+rotas.get('/postagens', postcontrolador.listar)
 
 
 export default rotas
